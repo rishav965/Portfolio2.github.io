db0eae4e333fc16c3f34096b5cd4f05056a7ee92
Rishav Kumar <96387480+rishav965@users.noreply.github.com>
2025-01-05 18:33:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ document.getElementById('contactForm').addEventListener('submit', function (even
 
 
 var typed = new Typed(".multiple-text", {
-    strings: ["Tech Enthusiast", "Tech Enthusiast", "Front-End Developer"],
+    strings: ["Tech Enthusiast", "Tech Enthusiast", "Front-End Developer", "Python Coder"],
     typeSpeed: 100,
     backSpeed: 100, 
     backdelay: 1000, 
